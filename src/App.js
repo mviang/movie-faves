21930088d99484f6c48a094b2c30cd5a886bdacc
Mary Jiang <mvj6@duke.edu>
2021-01-08 00:08:21 -0700

    integrated API call to return movies with hard-coded search val

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -7,20 +7,25 @@ import MovieList from './components/MovieList';
 
 const App = () =>  {
 
-  const [movies, setMovies] = useState([
-    {
-    "Title":"Harry Potter",
-    "Rating":"10",
-    },
-    {
-      "Title":"Star Wars",
-      "Rating":"10",
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const url = 'http://www.omdbapi.com/?s=harry potter&apikey=dc007d29';
+    const res = await fetch(url);
+    const resJson = await res.json();
+
+    if (resJson.Search) {
+      setMovies(resJson.Search);
     }
+  };
 
-]);
+  useEffect(() => {
+		getMovies();
+	}, []);
 
+  
   return (
-		<div className='container-fluid movie-app'>
+		<div className='container-fluid'>
 			<div className='row'>
 				<MovieList movies={movies} />
 			</div>
